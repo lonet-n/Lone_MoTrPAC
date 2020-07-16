b61e847a32321859397483be266844b4262ba2d2
Talha Numan Lone <lonet@smsh11dsu-srcf-d15-38.scg.stanford.edu>
2020-07-16 14:56:05 -0700

    This makes the gastrocnemius.txt and junc_path.txt files.

--- groups.sh
+++ groups.sh
@@ -1,9 +1,18 @@
+for sample in 90001015505 90015015505 90033015505 90037015505 90109015505 90123015505 90127015505 90141015505 90145015505 90159015505 90011015505 90029015505 90047015505 90119015505 90155015505 90013015505 90031015505 90121015505 90139015505 90157015505; do
+
+outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
+
+echo ${outdir}/${sample}Aligned.out.bam.junc >> ${outdir}/../junc_path.txt
+
+done
+
+
 for sample in 90001015505 90015015505 90033015505 90037015505 90109015505 90123015505 90127015505 90141015505\
     90145015505 90159015505; do
 
 outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
 
-echo ${sample}_Aligned.out.bam control >> ${outdir}/../gastrocnemius.txt
+echo ${sample}Aligned.out.bam control >> ${outdir}/../gastrocnemius.txt
 
 done
 
@@ -12,7 +21,7 @@ for sample in  90011015505 90029015505 90047015505 90119015505 90155015505; do
 
 outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
 
-echo ${sample}_Aligned.out.bam one_hour >> ${outdir}/../gastrocnemius.txt
+echo ${sample}Aligned.out.bam one_hour >> ${outdir}/../gastrocnemius.txt
 
 done
 
@@ -21,6 +30,6 @@ for sample in  90013015505 90031015505 90121015505 90139015505 90157015505; do
 
 outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
 
-echo ${sample}_Aligned.out.bam four_hour >> ${outdir}/../gastrocnemius.txt
+echo ${sample}Aligned.out.bam four_hour >> ${outdir}/../gastrocnemius.txt
 
 done
