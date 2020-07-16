72bc3c1fb362815c46e18a2d0c3e350655605b4b
Talha Numan Lone <lonet@smsh11dsu-srcf-d15-38.scg.stanford.edu>
2020-07-16 10:25:01 -0700

    This converts the .bam to .junc

--- bamjuncconv.sh
+++ bamjuncconv.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+#SBATCH -A default
+#SBATCH -p interactive
+#SBATCH -c 4
+#SBATCH --mem=40G
+#SBATCH -t 1-00:00:00
+set -e
+
 module load STAR/2.7.0d
 module load legacy
 module load leafcutter
 
-sample=80001995526
-# This is the first sample we will be testing– the code will eventually be parallelized so each sample is not completed individually.
-gindex=/oak/stanford/groups/smontgom/nicolerg/MOTRPAC/RNA/REFERENCES/rn6_ensembl_r95/star_index
-# This is the path to the STAR index.
-base=/projects/motrpac/PASS1A/RNA/NOVASEQ_BATCH1/from_bic
-# This is the path to the directory with the STAR Align and FastQ files.
 outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
-# This is the directory where all the files will output.
-leafcutter=/scg/apps/legacy/software/leafcutter/0.2.7/leafcutter
 
 for bamfile in `ls ${outdir}/${sample}*.bam`
 do
