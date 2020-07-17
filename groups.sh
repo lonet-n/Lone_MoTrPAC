4829c0ca8e420b23ee275f0c77c68020f29aa36e
Talha Numan Lone <lonet@smsh11dsu-srcf-d15-38.scg.stanford.edu>
2020-07-17 11:59:13 -0700

    Creates junc_path.txt and gastrocnemiusc#.txt files.

--- groups.sh
+++ groups.sh
@@ -2,7 +2,7 @@ for sample in 90001015505 90015015505 90033015505 90037015505 90109015505 901230
 
 outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
 
-echo ${outdir}/${sample}Aligned.out.bam.junc >> ${outdir}/../junc_path.txt
+echo ${outdir}/${sample}Aligned.out.bam.junc >> ${outdir}/../junc_paths.txt
 
 done
 
@@ -12,7 +12,7 @@ for sample in 90001015505 90015015505 90033015505 90037015505 90109015505 901230
 
 outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
 
-echo ${sample}Aligned.out.bam control >> ${outdir}/../gastrocnemius.txt
+echo ${sample}Aligned.out.bam control >> ${outdir}/../gastrocnemiusc1.txt
 
 done
 
@@ -21,7 +21,17 @@ for sample in  90011015505 90029015505 90047015505 90119015505 90155015505; do
 
 outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
 
-echo ${sample}Aligned.out.bam one_hour >> ${outdir}/../gastrocnemius.txt
+echo ${sample}Aligned.out.bam one_hour >> ${outdir}/../gastrocnemiusc1.txt
+
+done
+
+
+for sample in 90001015505 90015015505 90033015505 90037015505 90109015505 90123015505 90127015505 90141015505\
+    90145015505 90159015505; do
+
+outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
+
+echo ${sample}Aligned.out.bam control >> ${outdir}/../gastrocnemiusc4.txt
 
 done
 
@@ -30,6 +40,6 @@ for sample in  90013015505 90031015505 90121015505 90139015505 90157015505; do
 
 outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/${sample}
 
-echo ${sample}Aligned.out.bam four_hour >> ${outdir}/../gastrocnemius.txt
+echo ${sample}Aligned.out.bam four_hour >> ${outdir}/../gastrocnemiusc4.txt
 
 done
