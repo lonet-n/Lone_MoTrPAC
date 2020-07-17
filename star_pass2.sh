8fd0f71f32cd566aa2c4e6534fe373b07ddc78ba
lonet-n <66861525+lonet-n@users.noreply.github.com>
2020-07-17 16:11:11 -0400

    Update star_pass2.sh

--- star_pass2.sh
+++ star_pass2.sh
@@ -6,6 +6,23 @@
 #SBATCH -t 8:00:00
 set -e 
 
+
+# for sample in 90001015505 90015015505 90033015505 90037015505 90109015505 90123015505 90127015505 90141015505\
+#	90145015505 90159015505 90011015505 90029015505 90047015505 90119015505 90155015505 90013015505 90031015505\
+#	90121015505 90139015505 90157015505; do 
+#	sbatch --export=sample=${sample} star_pass2.sh
+# done
+# This is the code for gene alignment of control 0h, control 7h, exercise 1h, and exercise 4h data from the gastrocnemius male rats.
+
+# for sample in 90009015505 90028015505 90046015505 90118015505 90136015505 \
+#	90007015505 90025015505 90043015505 90115015505 90133015505 \
+#	90017015505 90053015505 90143015505 \
+#	90039015505 90129015505 90147015505 \
+#	90005015505 90023015505 90041015505; do
+#	sbatch --export=sample=${sample} star_pass2.sh
+# done
+# This is the code for gene alignment of exercise 0h, exercise 0.5h, exercise 7h, exercise 24h, and exercise 48h data from the gastrocnemius male rats.
+
 module load STAR/2.7.0d
 module load legacy
 module load leafcutter
