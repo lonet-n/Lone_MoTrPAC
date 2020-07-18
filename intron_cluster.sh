a439653fb1f3a36b496cd3421fa5de030c35521a
lonet-n <66861525+lonet-n@users.noreply.github.com>
2020-07-18 16:44:45 -0400

    Update intron_cluster.sh

--- intron_cluster.sh
+++ intron_cluster.sh
@@ -4,12 +4,12 @@ module load legacy
 module load leafcutter
 
 leafcutter=/scg/apps/legacy/software/leafcutter/0.2.7/leafcutter # This is a path to the Leafcutter folder.
-outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass
+outdir=/oak/stanford/groups/smontgom/lonet/motrpac/
 intron_prefix=gastroc_male
 
 mkdir -p ${outdir}/${intron_prefix}_intron_cluster # This will make a directory that will house all of the files produced.
 
-python ${leafcutter}/clustering/leafcutter_cluster.py -j ${outdir}/junc_paths.txt -r ${outdir}/${intron_prefix}_intron_cluster -m 50 -o ${intron_prefix} -l 50000
+python ${leafcutter}/clustering/leafcutter_cluster.py -j ${outdir}/star_secondpass/junc_paths.txt -r ${outdir}/${intron_prefix}_intron_cluster -m 50 -o ${intron_prefix} -l 50000
 # This clusters the introns in the junc files listed in junc_path.txt together.
 # This requires a 50 split read supporting each cluster.
 # This allows introns of up to 500kb.
