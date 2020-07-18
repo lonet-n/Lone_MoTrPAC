5f2eddb60beb1da08653638f28620aad13da37db
Talha Numan Lone <lonet@smsh11dsu-srcf-d15-38.scg.stanford.edu>
2020-07-18 15:04:44 -0700

    Fixed path issues.

--- ds_plot.sh
+++ ds_plot.sh
@@ -4,7 +4,7 @@ module loadd legacy
 module load leafcutter
 
 leafcutter=/scg/apps/legacy/software/leafcutter/0.2.7/leafcutter
-outdir=/oak/stanford/groups/smontgom/lonet/motrpac/star_secondpass/
+outdir=/oak/stanford/groups/smontgom/lonet/motrpac/
 
-${leafcutter}/scripts/ds_plots.R -e ${outdir}/rn6_exons.txt.gz ${outdir}/*_perind_numers.counts.gz ${outdir}/gastroc_male_cv().txt leafcutter_ds_cluster_significance.txt -f 0.05
-# The () is meant to be filled with 0, half, 1, 4, 7, 24, 48.
+${leafcutter}/scripts/ds_plots.R -e ${outdir}/rn6_exons.txt.gz ${outdir}/gastroc_male_intron_cluster/*_perind_numers.counts.gz \
+${outdir}/gastroc_male_ds_analysis/.txt ${outdir}/gastroc_male_ds_analysis/leafcutter_ds_cluster_significance.txt -f 0.05
